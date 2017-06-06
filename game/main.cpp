521931946c7e0f2b9c3883bc59b470ae935e1fad
MaxLin <maxylin@gmail.com>
2017-06-06 23:22:21 +0800

    Testing hand sort and deal

--- game/main.cpp
+++ game/main.cpp
@@ -5,7 +5,18 @@
 
 #include <iostream>
 
+#include "card.hpp"
+#include "deck.hpp"
+#include "hand.hpp"
+
 int main() {
     std::cout << "BigTwo" << std::endl;
+    Deck deck;
+    Hand hand;
+    for(int i = 0; i < 13; i++) {
+    	deck.Deal(hand);
+	}
+	hand.sortHand();
+	hand.printHand();
     return 0;
 }
